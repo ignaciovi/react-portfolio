29ae30fbe154d7969541ffc2f2225076095f7888
ignaciovi <ignvaldelvira@gmail.com>
2020-03-22 19:46:08 +0000

    Footer and Contact remove svg

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -43,12 +43,7 @@ class Contact extends Component {
 
 	return(
     <section className="site-section" id="contact">
-    <div className="container"> 
-        <div className="site-section__icon">
-          <svg xmlns="http://www.w3.org/2000/svg" width="33" height="7" viewBox="0 0 73.94 14.19">
-            <polyline points="2.83 2.83 11.36 11.36 19.9 2.83 28.43 11.36 36.97 2.83 45.5 11.36 54.04 2.83 62.58 11.36 71.11 2.83" style={{ strokeDashoffset: "2e-05", strokeDasharray: "none" }} ></polyline>
-          </svg>
-        </div>          
+    <div className="container">          
         <h1 className="site-heading"> Contact </h1>
         <Formik
           className="site-form"
