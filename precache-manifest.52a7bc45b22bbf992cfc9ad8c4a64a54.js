1b80bd9bb1114ddf4ecadd7ed5614e46dddc4fe6
ignaciovi <ignvaldelvira@gmail.com>
2020-03-23 22:06:56 +0000

    Updates

---
+++ precache-manifest.52a7bc45b22bbf992cfc9ad8c4a64a54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7371829ede9ce9af7a82760b885a605",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80f4dec2e98102dc0a1a",
+    "url": "/static/css/2.bd39f990.chunk.css"
+  },
+  {
+    "revision": "c8cf042e85f9c7a63120",
+    "url": "/static/css/main.e61c69a9.chunk.css"
+  },
+  {
+    "revision": "80f4dec2e98102dc0a1a",
+    "url": "/static/js/2.346d8a9e.chunk.js"
+  },
+  {
+    "revision": "c8cf042e85f9c7a63120",
+    "url": "/static/js/main.eccd866d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b904dcdc0a6868524f4c7b580f60e912",
+    "url": "/static/media/intro-logo.b904dcdc.png"
+  }
+]);
